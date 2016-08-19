1d98f3e10dec82c8c727304bd650dd8f31b643cb
MrChoiSSi <cjh890129@lycos.co.kr>
2016-08-19 17:05:51 +0900

    New changelist

--- common/js/ui.js
+++ common/js/ui.js
@@ -10,43 +10,43 @@ app.setDataList = function(){
 };
 
 app.handleGnbClick = function(){
-    var key = $(this).data("key");
+    app.key = $(this).data("key");
     app.$gnbBtn.parents("li").removeClass("current");
     $(this).parents("li").addClass("current");
     $(".viewWrap").find("ul").remove();
-    switch(key){
+    switch(app.key){
         case 0 :
             $.getJSON( "https://yoyo-mabyung.c9users.io/postManager/humor", function( data ){
                 console.log("humor");
-                app.initHtmlList(data);
+                app.initHtmlList(data, app.key);
             });
             break;
 
         case 1 :
             $.getJSON( "https://yoyo-mabyung.c9users.io/postManager/girls", function( data ){
                 console.log("girls");
-                app.initHtmlList(data);
+                app.initHtmlList(data, app.key);
             });
             break;
 
         case 2 :
             $.getJSON( "https://yoyo-mabyung.c9users.io/postManager/cam", function( data ){
                 console.log("cam");
-                app.initHtmlList(data);
+                app.initHtmlList(data, app.key);
             });
             break;
 
         case 3 :
             $.getJSON( "https://yoyo-mabyung.c9users.io/postManager/animal", function( data ){
                 console.log("animal");
-                app.initHtmlList(data);
+                app.initHtmlList(data, app.key);
             });
             break;
     }
     return false;
 };
 
-app.initHtmlList = function(data){
+app.initHtmlList = function(data, key){
     app.data = data;
     app.dataLength = app.data.length;
     var listHtml = "";
@@ -55,7 +55,11 @@ app.initHtmlList = function(data){
         listHtml += "<li data-key="+ i +">";
         listHtml += "<a href='#'>";
         listHtml += "<div class='imgWrap'>";
-        listHtml += "<img src="+ app.data[i].thumb +" alt='' />";
+        if(key === 2){
+            listHtml += "<img src="+ app.data[i].thumbURL +" alt='' />";
+        } else {
+            listHtml += "<img src="+ app.data[i].thumb +" alt='' />";
+        }
         listHtml += "</div>";
         listHtml += "<div class='txtWrap'>";
         listHtml += "<p class='tit'>"+ app.data[i].title +"</p>";
@@ -92,11 +96,13 @@ app.handleDetailViewClick = function(){
     app.$popConWrap = app.$popWrap.find(".conWrap");
     app.$dim = app.$body.find(".dim");
     app.$popTit.html(app.data[key].title);
-    for(var i =0; i < app.data[key].imgList.split("///").length; i++){
-        app.$popConWrap.append("<p><img src="+ app.data[key].imgList.split("///")[i] +" src='' /></p>");
-    }
-    if(key === 2){
-        app.$popConWrap.html("<p><iframe width='560' height='315' src='https://www.youtube.com/embed/"+ app.data[key].videoID +"' frameborder='0' allowfullscreen></iframe></p>");
+
+    if(app.key === 2){
+        app.$popConWrap.html("<p><iframe width='700' height='500' src='https://www.youtube.com/embed/"+ app.data[key].videoID +"' frameborder='0' allowfullscreen></iframe></p>");
+    } else{
+        for(var i =0; i < app.data[key].imgList.split("///").length; i++){
+            app.$popConWrap.append("<p><img src="+ app.data[key].imgList.split("///")[i] +" src='' /></p>");
+        }
     }
     setTimeout(app.handlePopAlign, 1000);
     return false;
