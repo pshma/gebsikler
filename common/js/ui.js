cf004e492aee27b06e3564bcc6b4ab4404d71af3
MrChoiSSi <cjh890129@lycos.co.kr>
2016-08-19 15:54:14 +0900

    base

--- common/js/ui.js
+++ common/js/ui.js
@@ -104,7 +104,7 @@ app.handleDetailViewClick = function(){
     app.$body.append(popHtml);
     app.$popWrap = app.$body.find(".popWrap");
     app.$dim = app.$body.find(".dim");
-    app.handlePopAlign();
+    setTimeout(app.handlePopAlign, 300);
     return false;
 };
 
@@ -113,6 +113,7 @@ app.handlePopAlign = function(){
     var winH = $(window).height();
     var popW = app.$popWrap.width();
     var popH = app.$popWrap.height();
+    console.log(popW)
     app.$popWrap.css({ "top" : parseInt((winH - popH) *.5), "left" : parseInt((winW - popW) *.5) });
 };
 
